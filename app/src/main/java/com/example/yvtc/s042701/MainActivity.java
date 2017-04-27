513eaea18571807f6f10ddd8e89c88575699d9d9
dannielccw <dannielccw@gmail.com>
2017-04-27 13:21:10 +0800

    + 鍵值

--- app/src/main/java/com/example/yvtc/s042701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s042701/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yvtc.s042701;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.StringBufferInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent it =getIntent();
+        String msg = it.getStringExtra("msg");
+        if (msg !=null )
+        {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            tv.setText(msg);
+        }
     }
 }
