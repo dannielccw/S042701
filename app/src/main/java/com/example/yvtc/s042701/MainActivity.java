333d7d6cebabab5a6c603ac0f859c7a8131f52ee
dannielccw <dannielccw@gmail.com>
2017-04-27 14:14:25 +0800

    + addValueEventListener (只要value值有改變, 就會顯示在TextView. App第一次登入時也會抓新的值)

--- app/src/main/java/com/example/yvtc/s042701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s042701/MainActivity.java
@@ -7,32 +7,50 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.StringBufferInputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Write a message to the database
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent it =getIntent();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+        Intent it = getIntent();
         String msg = it.getStringExtra("msg");
-        if (msg !=null )
+        if (msg != null)
         {
             TextView tv = (TextView) findViewById(R.id.textView);
             tv.setText(msg);
         }
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView tv2 = (TextView) findViewById(R.id.textView2);
+                String value = dataSnapshot.getValue(String.class);
+                tv2.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void clickSave(View v)
     {
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+
         EditText ed = (EditText) findViewById(R.id.editText);
         myRef.setValue(ed.getText().toString());
     }
