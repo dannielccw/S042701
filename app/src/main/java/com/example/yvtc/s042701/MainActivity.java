c36379361b1b4b152f834e9c41423fc8ae7537d9
dannielccw <dannielccw@gmail.com>
2017-04-27 15:28:13 +0800

    firebase Auth

--- app/src/main/java/com/example/yvtc/s042701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s042701/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.yvtc.s042701;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,12 +25,16 @@ public class MainActivity extends AppCompatActivity {
     // Write a message to the database
     FirebaseDatabase database;
     DatabaseReference myRef;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("message");
+        mAuth = FirebaseAuth.getInstance();
+
         Intent it = getIntent();
         String msg = it.getStringExtra("msg");
         if (msg != null)
@@ -53,5 +63,22 @@ public class MainActivity extends AppCompatActivity {
 
         EditText ed = (EditText) findViewById(R.id.editText);
         myRef.setValue(ed.getText().toString());
+    }
+
+    public void clickLogin(View v)
+    {
+        mAuth.signInWithEmailAndPassword("aa@aa.com", "123456").addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this, "登入成功", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "登入失敗", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/yvtc/s042701/MyApplication.java
@@ -0,0 +1,12 @@
+package com.example.yvtc.s042701;
+
+import android.app.Application;
+
+/**
+ * Created by YVTC on 2017/4/27.
+ */
+
+public class MyApplication extends Application {
+    public String uid;
+}
+
